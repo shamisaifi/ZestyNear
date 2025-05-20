95d7af2679ace663fec0eaed3ec500b9e4f503fe
shamisaifi <shamisaifi2003.com>
2025-05-20 11:40:21 +0530

    add port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`server running on port: ${PORT}`);
