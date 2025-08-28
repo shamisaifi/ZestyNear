a39d487bcd83dc3d2b257e3151d190cd216d7f51
shamisaifi <shamisaifi2003.com>
2025-08-28 21:20:13 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`server running on port: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`server running on port: ${PORT}`);
+// });
+export default app;
